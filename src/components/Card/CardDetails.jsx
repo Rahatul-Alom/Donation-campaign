9f3b48640c1c390f758a83551dc1adff3ba2fc79
Rahatul-Alom <muhammadrahatulalom@gmail.com>
2023-09-26 17:36:16 +0600

    added Dontion button color dynamicly

--- src/components/Card/CardDetails.jsx
+++ src/components/Card/CardDetails.jsx
@@ -2,13 +2,18 @@
 const CardDetails = ({card}) => {
     const {id, picture, title, category, category_bg, card_bg, text_button_bg, description } = card;
     return (
-        <div className="py-12">
+        <>
+         <div className="py-12 relative">
             <img className="w-full" src={picture} alt="" />
-            <div className="mt-12">
+            <div className="absolute w-full h-36 bg-black opacity-40 -mt-36">
+            </div>
+                <button className="btn absolute -mt-24 ml-10 border-none text-white"style={{ backgroundColor: `${text_button_bg}` }} >Donate $290</button>
+        </div>  
+        <div className="mt-7 px-9 md:px-9 lg:px-1">
                 <h1 className="text-4xl font-bold mb-5 ">{title}</h1>
                 <p className="text-justify" >{description}</p>
-            </div>
         </div>
+        </>
     );
 };
 
